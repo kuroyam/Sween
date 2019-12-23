378b21be7f6f6889479edbf374b7d25707a24643
Shun Kuroda <kuroyam1109@gmail.com>
2019-12-23 15:49:05 +0900

    Revert "Improve interface for flexibility."
    This reverts commit 3de3bda70c19b0a5e90132d1cc75ce8f729e30e5.

--- Sween/Sween.swift
+++ Sween/Sween.swift
@@ -9,23 +9,80 @@
 import QuartzCore
 import UIKit
 
-public class Sween<Action: Sweenable> {
+public class Sween {
+    
+    class Loop {
+        
+        static let infiniteLoop = -1
+        
+        let loopCount: Int
+        let interval: Double
+        
+        private var counter = 0
+        
+        init(count: Int, interval: Double) {
+            self.loopCount = count
+            self.interval  = interval
+        }
+        
+        var completed: Bool {
+            if loopCount == Loop.infiniteLoop {
+                return false
+            }
+            return counter >= loopCount
+        }
+        
+        func increment() {
+            if counter == Int.max {
+                counter = 0
+            }
+            counter += 1
+        }
+        
+    }
+    
+    private var loop = Sween.Loop(count: 0, interval: 0)
+    
+    private let view:     UIView
+    private var fromRect: CGRect
+    private var toRect:   CGRect
+    private var duration: Double = 0.1
+    private var easing:   Easing.Curve = Linear.Default
     
     private var displayLink: CADisplayLink?
     private var startTimestamp: CFTimeInterval = 0
     
-    private let action: Action
-    private var loop = Loop(count: 0, interval: 0)
-    
-    public init(with action: Action) {
-        self.action = action
+    public init(with view: UIView) {
+        self.view = view
+        fromRect  = view.frame
+        toRect    = view.frame
     }
     
     deinit {
         displayLink?.invalidate()
     }
     
-    public func loop(count: Int = Loop.infiniteLoop, interval: Double = 0.0) -> Sween {
+    public func duration(_ duration: Double) -> Sween {
+        self.duration = duration
+        return self
+    }
+    
+    public func transform(to rect: CGRect) -> Sween {
+        toRect = rect
+        return self
+    }
+    
+    public func move(to point: CGPoint) -> Sween {
+        toRect = CGRect(origin: point, size: toRect.size)
+        return self
+    }
+    
+    public func ease(_ easing: @escaping Easing.Curve) -> Sween {
+        self.easing = easing
+        return self
+    }
+    
+    public func loop(count: Int = Sween.Loop.infiniteLoop, interval: Double = 0.0) -> Sween {
         loop = Loop(count: count, interval: interval)
         return self
     }
@@ -45,11 +102,15 @@ public class Sween<Action: Sweenable> {
     }
     
     private func update(with time: CFTimeInterval) {
-        let current = time >= action.duration ? action.duration : time
+        let current = time >= duration ? duration : time
+        view.frame = CGRect(
+            x:      easing(current, Double(fromRect.origin.x), Double(toRect.origin.x - fromRect.origin.x), duration),
+            y:      easing(current, Double(fromRect.origin.y), Double(toRect.origin.y - fromRect.origin.y), duration),
+            width:  easing(current, Double(fromRect.width),    Double(toRect.width    - fromRect.width),    duration),
+            height: easing(current, Double(fromRect.height),   Double(toRect.height   - fromRect.height),   duration)
+        )
         
-        action.update(with: current)
-        
-        if time > action.duration + loop.interval {
+        if time > duration + loop.interval {
             loop.increment()
             if loop.completed {
                 displayLink?.invalidate()
--- SweenDemo/SweenDemo/ViewController.swift
+++ SweenDemo/SweenDemo/ViewController.swift
@@ -14,89 +14,81 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Linear
         let linearView = UIView(frame: CGRect(x: 50.0, y: 80.0, width: 10.0, height: 10.0))
         linearView.backgroundColor = UIColor.blue
         view.addSubview(linearView)
         
-        let linearTransform = Transform(with: linearView)
+        Sween(with: linearView)
+            .move(to: CGPoint(x: 325.0, y: 80.0))
             .duration(1.0)
-            .to(CGPoint(x: 325.0, y: 80.0))
-            .ease(Linear.Default)
+            .loop(interval: 1.0)
+            .animate()
         
-        Sween(with: linearTransform).loop(interval: 1.0).animate()
-        
-        // InQuad
         let inQuadView = UIView(frame: CGRect(x: 50.0, y: 100.0, width: 10.0, height: 10.0))
         inQuadView.backgroundColor = UIColor.blue
         view.addSubview(inQuadView)
         
-        let inQuadTransform = Transform(with: inQuadView)
+        Sween(with: inQuadView)
+            .move(to: CGPoint(x: 325.0, y: 100.0))
             .duration(1.0)
-            .to(CGPoint(x: 325.0, y: 100.0))
             .ease(Quad.In)
+            .loop(interval: 1.0)
+            .animate()
         
-        Sween(with: inQuadTransform).loop(interval: 1.0).animate()
-        
-        // OutQuad
         let outQuadView = UIView(frame: CGRect(x: 50.0, y: 120.0, width: 10.0, height: 10.0))
         outQuadView.backgroundColor = UIColor.blue
         view.addSubview(outQuadView)
         
-        let outQuadTransform = Transform(with: outQuadView)
+        Sween(with: outQuadView)
+            .move(to: CGPoint(x: 325.0, y: 120.0))
             .duration(1.0)
-            .to(CGPoint(x: 325.0, y: 120.0))
             .ease(Quad.Out)
+            .loop(interval: 1.0)
+            .animate()
         
-        Sween(with: outQuadTransform).loop(interval: 1.0).animate()
-        
-        // InOutQuad
         let inOutQuadView = UIView(frame: CGRect(x: 50.0, y: 140.0, width: 10.0, height: 10.0))
         inOutQuadView.backgroundColor = UIColor.blue
         view.addSubview(inOutQuadView)
         
-        let inOutQuadTransform = Transform(with: inOutQuadView)
+        Sween(with: inOutQuadView)
+            .move(to: CGPoint(x: 325.0, y: 140.0))
             .duration(1.0)
-            .to(CGPoint(x: 325.0, y: 140.0))
             .ease(Quad.InOut)
+            .loop(interval: 1.0)
+            .animate()
         
-        Sween(with: inOutQuadTransform).loop(interval: 1.0).animate()
-        
-        // InBack
         let inBackView = UIView(frame: CGRect(x: 50.0, y: 160.0, width: 10.0, height: 10.0))
         inBackView.backgroundColor = UIColor.blue
         view.addSubview(inBackView)
         
-        let inBackTransform = Transform(with: inBackView)
+        Sween(with: inBackView)
+            .move(to: CGPoint(x: 325.0, y: 160.0))
             .duration(1.0)
-            .to(CGPoint(x: 325.0, y: 160.0))
             .ease(Back.In)
+            .loop(interval: 1.0)
+            .animate()
         
-        Sween(with: inBackTransform).loop(interval: 1.0).animate()
-        
-        // OutBack
         let outBackView = UIView(frame: CGRect(x: 50.0, y: 180.0, width: 10.0, height: 10.0))
         outBackView.backgroundColor = UIColor.blue
         view.addSubview(outBackView)
         
-        let outBackTransform = Transform(with: outBackView)
+        Sween(with: outBackView)
+            .move(to: CGPoint(x: 325.0, y: 180.0))
             .duration(1.0)
-            .to(CGPoint(x: 325.0, y: 180.0))
             .ease(Back.Out)
+            .loop(interval: 1.0)
+            .animate()
         
-        Sween(with: outBackTransform).loop(interval: 1.0).animate()
-        
-        // InOutBack
         let inOutBackView = UIView(frame: CGRect(x: 50.0, y: 200.0, width: 10.0, height: 10.0))
         inOutBackView.backgroundColor = UIColor.blue
         view.addSubview(inOutBackView)
         
-        let inOutBackTransform = Transform(with: inOutBackView)
+        Sween(with: inOutBackView)
+            .move(to: CGPoint(x: 325.0, y: 200.0))
             .duration(1.0)
-            .to(CGPoint(x: 325.0, y: 200.0))
             .ease(Back.InOut)
-        
-        Sween(with: inOutBackTransform).loop(interval: 1.0).animate()
+            .loop(interval: 1.0)
+            .animate()
     }
     
 }
